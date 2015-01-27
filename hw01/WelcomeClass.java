3a67c409d712b2bc8ec2eb7e79601ad3ec43760a
paigejhowes <paigejhowes@gmail.com>
2015-01-27 21:03:10 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,22 @@
+//Paige Howes
+//Homework 01
+//Jan 27, 15
+
+//define a class
+public class WelcomeClass{
+    //add main method
+    public static void main(String[] args) {
+        //print welcome
+        System.out.println("-----------")
+        System.out.println("| WELCOME |")
+        System.out.println("-----------")
+        //print Lehigh network ID
+        System.out.println("  ^  ^  ^  ^  ^  ^  ")
+        System.out.println(" / \/ \/ \/ \/ \/ \ ")
+        System.out.println("<-P--J--H--2--1--7->")
+        System.out.println(" \ /\ /\ /\ /\ /\ / ")
+        System.out.println("  v  v  v  v  v  v  ")
+        //print short statement
+        System.out.println("I went to high school in Hollywood. I still don't know any celebrities.")
+    }
+}
