bd53bb2aef17b68d0ea06fdabd4b40b6b28fbb38
paigejhowes <paigejhowes@gmail.com>
2015-02-18 00:46:01 +0000

    updated WhichNumber

--- hw04/WhichNumber.java
+++ hw04/WhichNumber.java
@@ -14,41 +14,77 @@ public class WhichNumber {
         myScanner = new Scanner (System.in);
         
         //question one
+        System.out.println("Think of a number between 1 and 10 inclusive.");
         System.out.print("Is the number even? ");
         String even = myScanner.next();
         
         //split question one
-        if (even.equals("Y")) {
+        if (even.equals("y")) {
             //question 2 if answer was yes to question one
             System.out.print("Is the number divisible by 3? ");
             String three = myScanner.next();
-            if (three.equals("Y")) {
-                System.out.println("Your number is 6.");
+            if (three.equals("y")) {
+                System.out.println("Is your number 6? ");
+                String answer = myScanner.next();
+                if (answer.equals("y")) {
+                    System.out.println("Yay!");
+                }//end if statement
+                else {
+                    System.out.println("That is not correct.");
+                }//end else statement
             }//end if statement
             else {
                 //question 3 if answer was no to question two
                 System.out.print("Is your number divisible by 4? ");
                 String four = myScanner.next();
                 //question 4 if answer was yes to question 3
-                if (four.equals("Y")) {
+                if (four.equals("y")) {
                     System.out.print("Is the number divided by 4 greater than 1? ");
                     String one = myScanner.next();
-                    if (one.equals("Y")) {
-                        System.out.println("Your number is 8.");
+                    if (one.equals("y")) {
+                        System.out.println("Is your number 8?");
+                        String answer = myScanner.next();
+                        if (answer.equals("y")) {
+                            System.out.println("Yay!");
+                        }//end if statement
+                        else {
+                            System.out.println("That is not correct.");
+                        }//end else statement
                     }//end if statement
                     else {
-                        System.out.println("Your number is 4.");
+                        System.out.println("Is your number 4?");
+                        String answer = myScanner.next();
+                            if (answer.equals("y")) {
+                                System.out.println("Yay!");
+                            }//end if statement
+                            else {
+                                System.out.println("That is not correct.");
+                            }//end else statement
                     }//end else statement
                 }//end if statment
                 //question 4 if answer was no to question 3
                 else {
                     System.out.print("Is it divisible by 5? ");
                     String five = myScanner.next();
-                    if (five.equals("Y")){
-                        System.out.println("Your number is 10.");
+                    if (five.equals("y")){
+                        System.out.println("Is your number 10?");
+                        String answer = myScanner.next();
+                        if (answer.equals("y")) {
+                            System.out.println("Yay!");
+                        }//end if statement
+                        else {
+                            System.out.println("That is not correct.");
+                        }//end else statement
                     }//end if statement
                     else {
-                        System.out.println("Your number is 2.");
+                        System.out.println("Is your number 2?");
+                        String answer = myScanner.next();
+                        if (answer.equals("y")) {
+                            System.out.println("Yay!");
+                        }//end if statement
+                        else {
+                            System.out.println("That is not correct.");
+                        }//end else statement
                     }//end else statement
                 }//end else statement
             }//end else statement
@@ -58,32 +94,67 @@ public class WhichNumber {
             System.out.print("Is your number divisible by 3? ");
             String three = myScanner.next();
             //question if answer was yes to question 2
-            if (three.equals("Y")) {
+            if (three.equals("y")) {
                 System.out.print("When divided by 3 is the result greater than 1? ");
                 String one = myScanner.next();
-                if (one.equals("Y")) {
-                    System.out.println("Your number is 9.");
+                if (one.equals("y")) {
+                    System.out.println("Is your number 9?");
+                    String answer = myScanner.next();
+                    if (answer.equals("y")) {
+                        System.out.println("Yay!");
+                    }//end if statement
+                    else {
+                        System.out.println("That is not correct.");
+                    }//end else statement
                 }//end if statemnt
                 else {
-                    System.out.println("Your nubmer is 3.");
+                    System.out.println("Is your number 3?");
+                    String answer = myScanner.next();
+                    if (answer.equals("y")) {
+                        System.out.println("Yay!");
+                    }//end if statement
+                    else {
+                        System.out.println("That is not correct.");
+                    }//end else statement
                 }//end else statement
             }//end if statement
             //question if answer was no to question 2
             else {
                 System.out.print("Is it greater than 6? ");
                 String six = myScanner.next();
-                if (six.equals("Y")) {
-                    System.out.println("Your number is 7.");
+                if (six.equals("y")) {
+                    System.out.println("Is your number 7?");
+                    String answer = myScanner.next();
+                    if (answer.equals("y")) {
+                        System.out.println("Yay!");
+                    }//end if statement
+                    else {
+                        System.out.println("That is not correct.");
+                    }//end else statement
                 }//end if statement
                 //question if answer was no to question 3
                 else {
                     System.out.print("Is it less than 3? ");
                     String less = myScanner.next();
-                    if (less.equals("Y")) {
-                        System.out.println("Your number is 1.");
+                    if (less.equals("y")) {
+                        System.out.println("Is your number 1?");
+                        String answer = myScanner.next();
+                        if (answer.equals("y")) {
+                            System.out.println("Yay!");
+                        }//end if statement
+                        else {
+                            System.out.println("That is not correct.");
+                        }//end else statement
                     }//end if statement
                     else {
-                        System.out.println("Your number is 5.");
+                        System.out.println("Is your number 5?");
+                        String answer = myScanner.next();
+                        if (answer.equals("y")) {
+                            System.out.println("Yay!");
+                        }//end if statement
+                        else {
+                            System.out.println("That is not correct.");
+                        }//end else statement
                     }//end else statement
                 }//end else statement
             }//end else statement
