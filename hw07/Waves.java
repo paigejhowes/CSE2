6342d55f63c5e0904306c1e55dd118db782df755
paigejhowes <paigejhowes@gmail.com>
2015-03-24 19:22:46 +0000

    updated HW8

---
+++ hw07/Waves.java
@@ -0,0 +1,44 @@
+//Paige Howes
+//March 17, 2015
+//Homework #7
+
+//define class and method
+public class Waves {
+    public static void main (String [] args) {
+        
+        String temp = "";
+        for (int thirty = 0; thirty < 31; thirty++) {
+            for (int twentyNine = 0; twentyNine < 30; twentyNine++) {
+                for (int twentyEight = 0; twentyEight < 29; twentyEight++) {
+                    for (int twentySeven = 0; twentySeven < 29; twentySeven++) {
+                        for (int twentySix = 0; twentySix < 29; twentySix++) {
+                            for (int twentyFive = 0; twentyFive < 29; twentyFive++) {
+                                for (int twentyFour = 0; twentyFour < 29; twentyFour++) {
+                                    for (int twentyThree = 0; twentyThree < 29; twentyThree++) {
+                                        for (int twentyEight = 0; twentyEight < 29; twentyEight++) {
+                                            for (int twentyEight = 0; twentyEight < 29; twentyEight++) {
+                                                for (int twentyEight = 0; twentyEight < 29; twentyEight++) {
+                    
+                } 
+                }                               
+                }                           
+                                    }
+                                }
+                            }                    
+                        }
+                    }
+                }
+            }
+        }
+        
+        String temp2 = "";
+        int print2 = 2;
+        int two = 1;
+        while (two < 3) {
+            temp2 = temp2 + print2 + " ";
+            two++;
+            System.out.println(temp2);
+        }
+        
+    }//end of main method
+}//end of class
---
+++ hw08/HW8.java
@@ -0,0 +1,135 @@
+//Paige Howes
+//Homework #8
+//March 24,2015
+
+import java.util.Scanner;
+
+public class HW8 {
+    
+    public static void main(String[] args) {
+        char option;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Welcome to MG's adventure world. Now your journey begins. Good luck!");
+        System.out.println("Please hit 'C' or 'c' to continue, anything else to quit-");
+        String input = getInput(scan, "Cc");
+        System.out.println("You are in a dead valley.");
+        System.out.println("Please hit 'C' or 'c' to continue, anything else to quit-");
+        input = getInput(scan, "Cc");
+        System.out.println("You walked and walked and walked and you saw a cave!");
+        cave();
+        System.out.println("Please hit 'C' or 'c' to continue, anything else to quit-");
+        input = getInput(scan, "Cc");
+        System.out.println("You entered a cave!");
+        System.out.println("Please hit 'C' or 'c' to continue, anything else to quit-");
+        input = getInput(scan, "Cc");
+        System.out.println("Unfortunately, you ran into a GIANT!");
+        giant();
+        System.out.println("Enter 'A' or 'a' to Attack, 'E' or 'E' to Escape, ANYTHING else is to YIELD");
+        input = getInput(scan, "AaEe", 10);
+        System.out.println("Congratulations! You SURVIVED! Get your REWARD!");
+        System.out.println("There are three 3 tressure box in front of you! Enter the box number you want to open!");
+        box();
+        input = getInput(scan);
+        System.out.println("Hero! Have a good day!");
+    }
+    
+    public static void giant() {
+        System.out.println("                                 ---------                    ");
+        System.out.println("                                |  /    \\|                   ");
+        System.out.println("                      ZZZZZH    |    O    |    HZZZZZ             ");
+        System.out.println("                           H     -----------   H              ");
+        System.out.println("                      ZZZZZHHHHHHHHHHHHHHHHHHHHHZZZZZ                   ");
+        System.out.println("                           H    HHHHHHHHHHH    H                      ");
+        System.out.println("                      ZZZZZH    HHHHHHHHHHH    HZZZZZ               ");
+        System.out.println("                                HHHHHHHHHHH                   ");
+        System.out.println("                                HHH     HHH                   ");
+        System.out.println("                               HHH       HHH                   ");
+    }
+    
+    public static void cave() {
+         System.out.println("                              *****   * * * * * * * *        ");
+        System.out.println("                         ***         ***                      ");
+        System.out.println("                      ***               ***                   ");
+        System.out.println("                 |    ***               ***                   ");
+        System.out.println("                 |    ***               ***                   ");
+        System.out.println("             O __|__  ***               ***                   ");
+        System.out.println("           ******l    ***               ***                   ");
+        System.out.println("            * *       ***               ***                   ");
+        System.out.println("           *   *      ********************* * * * * * *       ");
+    }
+    
+    public static void box() {
+        System.out.println("                     *********************     *********************    *********************             ");
+        System.out.println("                     ***               ***     ***               ***    ***               ***              ");
+        System.out.println("                     ***       1       ***     ***       2       ***    ***       3       ***              ");
+        System.out.println("                     ***               ***     ***               ***    ***               ***              ");
+        System.out.println("                     *********************     *********************    *********************               "); 
+    }
+    
+    public static String getInput(Scanner scan, String string) {
+        String loser = "";
+        if (string != "c" || string != "C") {
+            loser = "Yea right LOSER!";
+        }
+        else {
+            loser = "";
+            System.exit(0);
+        }
+        System.out.println(loser);
+        return loser;
+    }
+    
+    public static String getInput(Scanner scan, String string, int trial) {
+        String hit = "";
+        if (string == "A" || string == "a") {
+            int success = (int) (Math.random());
+            int strike = 0;
+            if (success == 0) {
+                hit = "Gosh! How can you miss it!";
+            }
+            else {
+                System.out.println("Critical hit!");
+                strike++;
+                if (strike == 10) {
+                    hit = "Congratulations! You SURVIVED! Get your REWARD!";
+                }
+                else {
+                    hit = "";
+                }
+            }
+        }
+        else if (string == "E" || string == "e") {
+            int success = (int) (Math.random() * 10);
+            if (success == 10) {
+                hit = "Congratulations! You SURVIVED! Get your REWARD!";
+            }
+            else {
+                hit = "";
+            }
+        }
+        else {
+            hit = "Executed by the GIANT! Game over!";
+        }
+        System.out.println(hit);
+        return hit;
+    }
+    
+    public static String getInput(Scanner scan) {
+        int open = scan.nextInt();
+        String box = "";
+        switch (open) {
+            case 1:
+                box = "Wrong number! You get nothing! Better restart the game LOL";
+                break;
+            case 2:
+                box = "Congratulations! You get the treasure!";
+                break;
+            case 3:
+                box = "Wrong number! You get nothing! Better restart the game LOL";
+                break;
+        }
+        System.out.println(box);
+        return box;
+    }
+    
+}
