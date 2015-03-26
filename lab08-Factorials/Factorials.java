1ba22fa9de810fa95a0ae746281b761559b5aa75
paigejhowes <paigejhowes@gmail.com>
2015-03-26 22:13:33 +0000

    updated lab08-Factorials

--- lab08-Factorials/Factorials.java
+++ lab08-Factorials/Factorials.java
@@ -8,26 +8,24 @@ public class Factorials {
     
     public static void main (String[] args) {
         Scanner scan = new Scanner(System.in);
+        System.out.print("Enter what number you wish to have the factorial of ");
         int number = scan.nextInt();
         int sum = 0;
         for (int i = 1; i <= number; i++) {
             sum += factorial(i);
         }
+        print(number, sum);
     }
     
     public static int factorial (int input) {
-        
         int multiply = 1;
-        
         for (int j = 1; j <= input; j++) {
             multiply *= j;
         }
-        
-        System.out.println(multiply);
         return multiply;
     }
     
     public static void print (int input, int sum) {
-        System.out.println("The super factorial of " + input + " is equal to " + sum);
+        System.out.println("The super factorial of " + input + " is equal to " + sum + ".");
     }
 }
